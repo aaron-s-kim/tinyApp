3b48dc5b2341feaa50c222acf26413deb8870298
Aaron K <tonberryspawn@gmail.com>
2021-11-08 18:30:54 -0800

    added urls_index table

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set  view engine to ejs
+app.set('view engine', 'ejs'); // tells Express app to use EJS as templating engine
+// EJS knows to check views directory for template files with ext .ejs
+// vars sent to an EJS template must be inside an object
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,8 +16,9 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -20,5 +26,11 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
